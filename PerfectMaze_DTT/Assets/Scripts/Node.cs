193e5bbcdff2385cbf019ca6f5d5409e38347e6f
schlorp <70897090+schlorp@users.noreply.github.com>
2022-11-30 18:33:13 +0100

    completely finished

--- PerfectMaze_DTT/Assets/Scripts/Node.cs
+++ PerfectMaze_DTT/Assets/Scripts/Node.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Node : MonoBehaviour
 {
     public List<GameObject> walls = new List<GameObject>();
+    public GameObject floor;
     public void RemoveWall(int wallindex)
     {
         Destroy(walls[wallindex]);
